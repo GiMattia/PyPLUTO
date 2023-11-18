74b09fc5ae10e325f6bb76bcaf066b2d2577078f
Daniele Crocco <darticus1992@gmail.com>
2023-11-18 16:49:31 +0100

    noia

--- Src/pyPLUTO/__init__.py
+++ Src/pyPLUTO/__init__.py
@@ -1,70 +1,143 @@
-from ._libraries import *
+from .libraries import *
 
 class Load:
 
-    D = {}
+    '''
+    def __new__(cls, *args, **kwargs):
+        new_instance = super().__new__(cls)
 
-    def __new__(cls, nout = 'last', path = './' , datatype = None, 
-                     vars = True, text = True):
-        
-        cls.pathdir = Path(path)
-        if text is not False:
-            print(f'Loading folder:   {path}')
-        if text is True:
-            print(f'Preferred format: {datatype}')
-
-        cls.find_format(cls,datatype)
-        if datatype == None and text == True:
-            print(f'Format found:     {cls.format}')
-
-        cls.read_grid(cls)
-        cls.read_vars(cls,nout)   
-        if text is not False:
-            print('Dimensions:       '+str(cls.D['dim']))
-            print('Geometry:         '+str(cls.D['geom']))
-        if text is True:
-            print('Grid variables:   '+str(cls.gridlist1)[:-1]+',')
-            print('                   '+str(cls.gridlist2)[1:-1]+',')
-            try:
-                print('                  '+str(cls.gridlist3)[1:-1]+',')
-            except:
-                None
-            print('                   '+str(cls.gridlist4)[1:])
-            print('Time variables:   '+str(cls.addvarlist))
-
-        if isinstance(cls.D['nout'], np.ndarray):
-            cls.noutlist = cls.D['nout']
-            cls.varfiles = []
+        if kwargs.get('nout', 'last') is None:
+            return new_instance
         else:
-            cls.noutlist = np.atleast_1d(cls.D['nout'])
+            new_instance.__init__(nout = 'last', path = './' , datatype = None, 
+                     vars = True, text = True)
+            return new_instance
+    '''
 
-        cls.varlist = {}
-        for i, exout in enumerate(cls.noutlist):
-            cls.load_vars(cls,vars,i,exout,text)
+    def __init__(self, nout = 'last', path: str  = './' , datatype: str = None, 
+                       vars =  True,  text: bool = True, alone: bool = False):
+        '''
+        Initialization of the Load class.
+        The initialization corresponds to the loading, if wanted, of one or more
+        datafiles for the fluid.
+        The data are loaded in a numpy multidimensional array through memory mapping. Such approach
+        does not load the data until needed.
+        Basic operations (i.e. no numpy) are possible, as well as slicing the arrays, without
+        fully loading the data.
 
-        #if isinstance(cls.D['nout'], np.ndarray):
-        #    Path("combined_data.dat").unlink()
-        
-        return cls.Dict2Class(cls.D)
+        Returns
+        -------
 
-    class Dict2Class():
-        def __init__(self, my_dict):
-            for key in my_dict:
-                setattr(self, key, my_dict[key])
+            The class, with the grid and the loaded variables.
+
+        Parameters
+        ----------
+
+            - nout: int/str/list, default 'last'
+                The files to be loaded. Possible choices are int values (which
+                correspond to the number of the output file), strings ('last', which
+                corresponds to the last file, 'all', which corresponds to all files)
+                or a list of the aforementioned types.
+                Note that the 'all' value should be used carefully, e.g. when the
+                data need to be shown interactively.
+            - path: str, default'./'
+                The path of the folder where the files should be loaded.
+            - datatype: str, default None
+                The format of the data file. If not specified, the code will look for 
+                the format from the list ['dbl','vtk','flt'] in the following order.
+                HDF5 and tab formats have not been implemented yet.
+            - vars: str/list/bool, default True
+                The variables to be loaded. The default value, True, corresponds to all the
+                variables.
+            - text: bool, default True
+                If a quick text (explaining the path and few information) should be
+                shown. In case the user needs a more detailed information of the structure
+                and attributes loaded from the class, the __str__ method provides
+                a easy display of all the important information.
+        '''
+
+        self._check_pathformat(path)
+
+        if text is not False: print(f'Loading folder:   {path}')
+            
+        self._find_format(datatype, alone)
+
+        self._read_grid()
+        self._read_vars(nout)   
+        if text is not False: print(f'Dimensions: {self.dim} ({self.geom})')
     
-    from ._readout import find_format, read_grid, read_vars, load_vars
-    from ._h_load  import split_gridfile, rec_format, vtk_offset, gen_offset
-    from ._h_load  import check_nout, init_vardict, assign_var, shape_st
+        if isinstance(self.nout, np.ndarray):
+            self.noutlist = self.nout
+            self.varfiles = []
+        else:
+            self.noutlist = np.atleast_1d(self.nout)
 
+        self.varlist = {}
+        for i, exout in enumerate(self.noutlist):
+            self._load_vars(vars,i,exout,text)
+        
+        for key in self.D_vars:
+            setattr(self, key, self.D_vars[key])
+        if text is not False: print(f'Output loaded: {self.nout}')
+        
+
+        #self._delete_vars() 
+
+    def __str__(self):
+        text3 = f'''        - Cartesian projection              {['x1c','x2c','x1rc','x2rc']}\n''' 
+        text3 = text3 if self.geom != 'CARTESIAN' else ""
+
+        text = f'''
+        Load class.
+        It loads the data.
+
+        File properties:
+        - Current path loaded (pathdir)      {self.pathdir} 
+        - Format loaded       (format)       {self.format}
+
+        Simulation properties
+        - Dimensions    (dim)      {self.dim}
+        - Geometry      (geom)     {self.geom}
+        - Grid size     (gridsize) {self.gridsize}
+        - Grid shape    (nshp)     {self.nshp}
+        - Output loaded (nout)     {self.nout}
+        - Time loaded   (ntime)    {self.ntime}
+
+        Public attributes available:
+        - Number of cells in each direction {['nx1','nx2','nx3']}
+        - Grid values (cell center)         {['x1','x2','x3']}
+        - Grid values (face center)         {['x1r','x2r','x3r']}
+        - Cells size                        {['dx1','dx2','dx3']}
+        - Time attributes                   {['outlist','timelist']}\n{text3}        
+        Variables available:
+        {self.Dinfo['varslist'][0]}
+        Variables loaded: 
+        {self.load_vars}
+
+        Public methods available: WIP...
+
+        Please refrain from using "private" methods and attributes.
+        '''
+        return text
+    
+    from .readdata  import _check_pathformat, _find_format
+    from .readfluid import _find_formatout, _read_grid, _read_vars
+    from .readfluid import _load_vars, _delete_vars
+    from .h_load    import _split_gridfile, _rec_format, _vtk_offset, _gen_offset
+    from .h_load    import _check_nout, _init_vardict, _assign_var, _shape_st
+    
 class Image:
 
-    def __init__(self,LaTeX = True, text = False, fig = None, **kwargs):
+    def __init__(self,LaTeX = True, text: bool = False, fig = None, **kwargs):
 
-        self.fontsize = 17
-        self.tight    = True
-        self.nwin     = 1
-        self.figsize  = [8,5]
-        self.set_size = False
+       # self.assign_default()
+       # self.assign_LaTeX(LaTeX)
+
+        self.fontsize: int  = 17
+        self.tight:    bool = True
+        self.nwin:     int  = 1
+        self.figsize = [8,5]
+        self._set_size = False
         self.nrow0    = 0
         self.ncol0    = 0
         self.ax       = []
@@ -79,16 +152,6 @@ class Image:
         self.legpar   = []
         self.tickspar = []
         self.shade    = []
-        self.parax    = ['fontsize','xrange','yrange','title','xtitle','ytitle',
-                         'titlesize','labelsize','tickssize','xticks','yticks',
-                         'xtickslabels','ytickslabels','xscale','yscale','alpha',
-                         'ticksdir','minorticks','aspect']
-        self.parfig   = ['fig','tight','figsize','fontsize','nwin','suptitle']
-        self.parplot  = ['axes','c','ls','lw','marker','ms','label','fillstyle',
-                         'legend','legsize','legcols', 'pos']
-        self.pardis   = ['axes','x1', 'x2','vmin','vmax','shading','cmap','cbar',
-                         'clabel','cscale','cticks','ctickslabels','lint']
-        self.parzoom  = ['left', 'width', 'top', 'height', 'pos', 'ind']
         self.LaTeX    = LaTeX
 
         if LaTeX is True:
@@ -113,25 +176,61 @@ class Image:
                 "text.usetex": True
             })
 
-
-
-        if text == True:
-            print('Creating Figure')
-
         self.create_fig(fig, **kwargs)
+        if text is not False:
+            print(f'Creating Figure in window {self.nwin}')
 
-    from ._h_image  import place_inset_pos, place_inset_loc
-    from ._h_image  import set_parax, check_par, set_xrange, set_yrange
-    from ._h_image  import set_xticks, set_yticks, check_rows, check_cols
-    from ._h_image  import hide_text, set_cscale
-    from ._h_image  import check_fig, add_ax, assign_ax
+    def __str__(self):
+        plt.rcParams['text.usetex'] = True
+        plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+        return rf'''
+        Image class.
+        It plots the data.
 
-    from ._fig      import create_fig, create_axes, set_axis
-    from ._plot     import plot, legend
-    from ._display  import display, colorbar
-    from ._interact import interactive, update_slider
-    from ._figtools import savefig, show, text
-    from ._zoom     import zoom, zoomplot, zoomdisplay
+        Image properties:
+        - Figure size        (figsize)       {self.figsize}
+        - Window number      (nwin)          {self.nwin}
+        - Number of subplots (nrow0 x ncol0) {self.nrow0} x {self.ncol0}
+        - Global fontsize    (fontsize)      {self.fontsize}
+
+        Public attributes available: WIP...
+
+        Public methods available: 
+        - create_axes
+          It adds a set of [nrow,ncol] subplots to the figure.
+        - set_axis
+          Changes the parameter of a specific subplot
+        - plot
+          Plots one line in a subplot
+        - legend
+          Places a legend in a subplot
+        - display
+          Plots a 2D quantity in a subplot
+        - colorbar
+          Places a colorbar in a subplotor next to a subplot
+        - zoom
+          Creates an inset zom region of a subplot
+        - text
+          Places the text in the figure or in a subplot
+        - interactive
+          Creates a slider of a quantity as function of time
+        
+
+        Please refrain from using "private" methods and attributes.
+        '''
+
+    from .h_image  import _place_inset_pos, _place_inset_loc
+    from .h_image  import _set_parax, _check_par, _set_xrange, _set_yrange
+    from .h_image  import _set_xticks, _set_yticks, _check_rows, _check_cols
+    from .h_image  import _hide_text, _set_cscale
+    from .h_image  import _check_fig, _add_ax, _assign_ax
+
+    from .fig      import create_fig, create_axes, set_axis
+    from .plot     import plot, legend
+    from .display  import display, colorbar
+    from .interact import interactive, _update_slider
+    from .figtools import savefig, show, text
+    from .zoom     import zoom, _zoomplot, _zoomdisplay
 
     from .ploadparticles import ploadparticles
 
@@ -150,13 +249,25 @@ class Tools:
             except:
                 None
 
-    from ._datatools import slices, mirror
-    from ._nabla     import gradient
-    from ._lines     import fieldlines, field_interp, adv_field_line, check_closed_line
+    def __str__(self):
+        return f'''
+        Tools class.
+        It manipulates the data.
+        Attributes: blablabla
+        Methods available: blablablax2
+        Please refrain from using "private" methods.
+        '''
 
-from ._pytools   import savefig, show
+    from .datatools import slices, mirror
+    from .nabla     import gradient
+    from .lines     import fieldlines, field_interp, adv_field_line, check_closed_line
+
+from .pytools   import savefig, show
+
+
 
 class LoadParticles:
     def __new__(cls, nout = 'last', path = './' , datatype = None, 
                      vars = True, text = True):
+        
         None

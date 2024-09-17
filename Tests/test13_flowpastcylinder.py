216a82329934b959d47b51f1c532f644c9e5b953
GiMattia <mattiagiancarlo@gmail.com>
2024-09-17 20:01:49 +0200

    added comments test 13

--- Tests/test13_flowpastcylinder.py
+++ Tests/test13_flowpastcylinder.py
@@ -7,13 +7,18 @@ import matplotlib.pyplot as plt
 plutodir = os.environ['PLUTO_DIR']
 wdir     = plutodir + '/Test_Problems/HD/Viscosity/Flow_Past_Cylinder'
 
+# Load the data (4 levels of AMR)
 D = pp.Load(path = wdir, level = 4)
 
+# Convert the grid to cartesian
 rr, pphh = np.meshgrid(D.x1r,D.x2r,indexing='ij')
 xx = rr*np.cos(pphh)
 yy = rr*np.sin(pphh)
 
+# Initialize the Image class (with black color)
 I = pp.Image(withblack = True)
+
+# Show the 2D density
 I.display(D.rho, x1 = xx, x2 = yy, 
                  cpos = 'right', 
                  xrange = [-10,50], 
@@ -24,7 +29,10 @@ I.display(D.rho, x1 = xx, x2 = yy,
                  xtitle = 'x',
                  ytitle = 'y',
                  title = 'Test 13 - AMR Flow past cylinder test')
+                 
+# Plot the AMR levels (up to 2)
 I.oplotbox(D.AMRLevel,lrange=[0,2],geom=D.geom)
 
+# Save and show the figure
 I.savefig('test13_flowpastcylinder.png')
 pp.show()

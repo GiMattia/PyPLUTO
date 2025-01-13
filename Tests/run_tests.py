8c0821b0438520be6f4c07ee55e1b0d304c5bfcc
GiMattia <mattiagiancarlo@gmail.com>
2025-01-13 19:37:18 +0100

    chenged " '

--- Tests/run_tests.py
+++ Tests/run_tests.py
@@ -6,10 +6,10 @@ yes = "\033[32mENABLED\033[0m"
 no  = "\033[31mDISABLED\033[0m"
 
 options = {
-    f"{"Loading":<8} check":  (no if "noload"     in sys.argv else yes, "lf"),
-    f"{"LoadPart":<8} check": (no if "noloadpart" in sys.argv else yes, "lp"),
-    f"{"Image":<8} check":    (no if "noimage"    in sys.argv else yes, "im"),
-    f"{"Examples":<8} check": (no if "notests"    in sys.argv else yes, "te")}
+    f'{"Loading":<8} check':  (no if "noload"     in sys.argv else yes, "lf"),
+    f'{"LoadPart":<8} check': (no if "noloadpart" in sys.argv else yes, "lp"),
+    f'{"Image":<8} check':    (no if "noimage"    in sys.argv else yes, "im"),
+    f'{"Examples":<8} check': (no if "notests"    in sys.argv else yes, "te")}
 
 def single_test(file_path):
     try:
@@ -22,7 +22,7 @@ def single_test(file_path):
         return False
 
 if __name__ == "__main__":
-    print(f"Running tests with keywords: {', '.join(sys.argv[1:]) if len(sys.argv[1:])> 0 else "None"}")
+    print(f"Running tests with keywords: {', '.join(sys.argv[1:]) if len(sys.argv[1:])> 0 else 'None'}")
     index = []
     for option, status in options.items():
         print(f"{option}: {status[0]}")

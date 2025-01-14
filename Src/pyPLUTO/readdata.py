ff8a648dde7b6e1c0849492557f41412293557c3
GiMattia <mattiagiancarlo@gmail.com>
2025-01-14 16:46:51 +0100

    debug again

--- Src/pyPLUTO/readdata.py
+++ Src/pyPLUTO/readdata.py
@@ -65,8 +65,6 @@ def _load_variables(self,
     if class_name == 'Load':
         # If the class name is Load (single file), the filepath is data
         self._filepath = self.pathdir / ('data' + self._d_info['endpath'][i])
-        print(f"{self.pathdir=}")
-        print(f"{self._filepath}")
     elif class_name == 'LoadPart':
         # If the class name is LoadPart, the filepath is particles
         self._filepath = self.pathdir / ('particles' + \
@@ -142,6 +140,8 @@ def _load_variables(self,
     for j in self._load_vars:
         if self._d_info['typefile'][i] == 'multiple_files':
             self._filepath = self.pathdir / (j + self._d_info['endpath'][i])
+            print(f"{self.pathdir=}")
+            print(f"{self._filepath=}")
             self._compute_offset(i, endian, exout, j)
             start_byte = self._offset[j]
             # Reload memmap for the new file

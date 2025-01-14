61889315edf5d249ef3e7c3642fefc829f362d9a
GiMattia <mattiagiancarlo@gmail.com>
2025-01-14 15:40:24 +0100

    trypath

--- Tests/test_lf02_loadformat.py
+++ Tests/test_lf02_loadformat.py
@@ -5,27 +5,27 @@ from pathlib import Path
 
 # Assuming the root of the repo is your current working directory
 repo_root = Path(os.getcwd())
-path = repo_root / "Test_load"
+path = "Test_load"
 
 # Format not given (single file), finding dbl
 def test_single_finddbl():
-    D = pp.Load(path = "single_file", text = False)
+    D = pp.Load(path = path / "single_file", text = False)
     assert D.format == "dbl"
 
 # Format not given (single file), finding vtk
 def test_single_findvtk():
-    D = pp.Load(path = "single_file/vtk", text = False)
+    D = pp.Load(path = path / "single_file/vtk", text = False)
     assert D.format == "vtk"
 
 # Given format (single file), alone = False
 def test_find_singlegiven():
     for format in ["dbl","flt","vtk","dbl.h5","flt.h5"]:
-        D = pp.Load(path = "single_file", text = False, datatype = format)
+        D = pp.Load(path = path / "single_file", text = False, datatype = format)
         assert D.format == format
 
 # Given format (single file), alone = True
 def test_alone_vtk():
-    D = pp.Load(path = "single_file", text = False, datatype = 'vtk', alone = True)
+    D = pp.Load(path = path / "single_file", text = False, datatype = 'vtk', alone = True)
     assert D.format == 'vtk'
 
 # dbl.h5 and flt.h5 should raise a warning
@@ -35,34 +35,34 @@ def test_find_alone_h5():
             "the .out file is recommended.\n")
     for format in ["dbl.h5","flt.h5"]:
         with pytest.warns(UserWarning, match=warn):
-            D = pp.Load(path = "single_file", text = False, datatype = format, alone = True)
+            D = pp.Load(path = path / "single_file", text = False, datatype = format, alone = True)
             assert D.format == format
 
 # Format not given (multiple files), finding dbl
 def test_multiple_finddbl():
-    D = pp.Load(path = "multiple_files", text = False)
+    D = pp.Load(path = path / "multiple_files", text = False)
     assert D.format == "dbl"
 
 # Format not given (multiple files), finding vtk
 def test_multiple_findvtk():
-    D = pp.Load(path = "multiple_files/vtk", text = False)
+    D = pp.Load(path = path / "multiple_files/vtk", text = False)
     assert D.format == "vtk"
 
 # Given format (multiple files), alone = False
 def test_multiple_findformat():
     for format in ["dbl","flt","vtk"]:
-        D = pp.Load(path = "multiple_files", text = False, datatype = format)
+        D = pp.Load(path = path / "multiple_files", text = False, datatype = format)
         assert D.format == format
 
 # Given format (multiple files), alone = True
 def test_multiple_alone():
-    D = pp.Load(path = "multiple_files", text = False, datatype = 'vtk', alone = True)
+    D = pp.Load(path = "Test_load/multiple_files", text = False, datatype = 'vtk', alone = True)
     assert D.format == 'vtk'
 
 # Check if raises error if the format is wrong
 def test_wrong_format():
     with pytest.raises(ValueError):
-        D = pp.Load(path = "single_file", text = False, datatype = "wrong")
+        D = pp.Load(path = path / "single_file", text = False, datatype = "wrong")
 
 # Check if raises an error if there is no good format
 def test_noformat():
@@ -72,4 +72,4 @@ def test_noformat():
 # Check if raises error if the selected format does not exist
 def test_format_noexists():
     with pytest.raises(FileNotFoundError):
-        D = pp.Load(path = "multiple_files", text = False, datatype = "dbl.h5")
+        D = pp.Load(path = path / "multiple_files", text = False, datatype = "dbl.h5")
